2279fda4d4368b1f45986d84a8f9ed9b09f312df
Bogdan5 <bogdan5x@gmail.com>
2018-08-10 22:54:56 -0400

    another commit

--- download.js
+++ download.js
@@ -8,21 +8,23 @@ const downloadPage = (url='http://nodeprogram.com') => {
     http.get(urlF, (response) => {
       let buff = '';
       response.on('data', (chunk) => {
-        buff += chunk
+        buff += chunk;
       });
       response.on('end', () => {
-        callback(null, buff)
+        callback(null, buff);
       });
     }).on('error', (error) => {
-      console.error(`Got error: ${error.message}`)
-      callback(error)
+      console.error(`Got error: ${error.message}`);
+      callback(error);
     });
+  };
+
   const folderName = uuidv1();
+  fs.mkdirSync(folderName);
+  fetchPage(url, (error, data) => {
+    fs.writeFileSync(path.join(__dirname, folderName, 'file.html'), data);
+    console.log('downloading is done in folder ', folderName);
+  });
+};
 
-  fs.mkdirSync(folderName)
-  fetchPage(url, (error, data)=>{
-
-    fs.writeFileSync(path.join(__dirname, folderName, 'file.html'), data)
-    console.log('downloading is done in folder ', folderName)
-  })
-}
+downloadPage(process.argv[2]);
